597e655168046b6330225ba8b15d9e9b6ab629c6
dsteelemaley <44673794+dsteelemaley@users.noreply.github.com>
2018-11-10 18:06:32 -0500

    Update MyBot.py

--- Halite3_Python3_MacOS/MyBot.py
+++ Halite3_Python3_MacOS/MyBot.py
@@ -12,50 +12,54 @@ import logging
 directions = ["n","s","e","w"]
 game = hlt.Game()
 # Respond with your name.
-game.ready("half")
+game.ready("Good Sir")
 goback = {}
 yard = game.game_map[game.me.shipyard].position
 
 while True:
-	# Get the latest game state.
-	game.update_frame()
-	# You extract player metadata and the updated map metadata here for convenience.
-	me = game.me
-	game_map = game.game_map
-	
-	# A command queue holds all the commands you will run this turn.
-	command_queue = []
-	
-	for ship in me.get_ships():
-		if ship.halite_amount > 3*(constants.MAX_HALITE/5):
-			goback[ship.id] = True
-		elif game_map.calculate_distance(ship.position, yard) < 1:
-			goback[ship.id] = False
-	
-	for ship in me.get_ships():
-		if game.turn_number > constants.MAX_TURNS*(8/9):
-			try:
-				command_queue.append(ship.move(random.choice(game_map.get_unsafe_moves(ship.position, yard))))
-			except:
-				command_queue.append(ship.move("o"))
-		elif game_map[ship.position].halite_amount > constants.MAX_HALITE / 20 and ship.halite_amount < 8*(constants.MAX_HALITE/9):
-			command_queue.append(ship.stay_still())
-		elif goback[ship.id]:
-			command_queue.append(ship.move(game_map.naive_navigate(ship, yard)))
-		elif game_map[ship.position].halite_amount < constants.MAX_HALITE / 15:
-			for dir in random.sample(hlt.Direction.get_all_cardinals(),len(hlt.Direction.get_all_cardinals())):
-				if game_map.calculate_distance(ship.position.directional_offset(dir),yard) > game_map.calculate_distance(ship.position, yard):
-					command_queue.append(ship.move(game_map.naive_navigate(ship, ship.position.directional_offset(dir))))
-					break
-		else:
-			command_queue.append(ship.move(game_map.naive_navigate(ship, yard)))
-			
-		  # If you're on the first turn and have enough halite, spawn a ship.
-	# Don't spawn a ship if you currently have a ship at port, though.
-	if game.turn_number >= 1 and (me.halite_amount >= constants.SHIP_COST or (len(me.get_ships()) < 1 and me.halite_amount >= constants.SHIP_COST)) and not game_map[me.shipyard].is_occupied:
-			if game.turn_number < constants.MAX_TURNS/2:
-				command_queue.append(game.me.shipyard.spawn())
-			elif len(me.get_ships()) < 1:
-				command_queue.append(game.me.shipyard.spawn())
-	# Send your moves back to the game environment, ending this turn
-	game.end_turn(command_queue)
+    # Get the latest game state.
+    game.update_frame()
+    # You extract player metadata and the updated map metadata here for convenience.
+    me = game.me
+    game_map = game.game_map
+    
+    # A command queue holds all the commands you will run this turn.
+    command_queue = []
+    
+    for ship in me.get_ships():
+        if ship.halite_amount > 3*(constants.MAX_HALITE/5):
+            goback[ship.id] = True
+        elif game_map.calculate_distance(ship.position, yard) < 1:
+            goback[ship.id] = False
+    
+    for ship in me.get_ships():
+        if game.turn_number > constants.MAX_TURNS*(8/9):
+            try:
+                command_queue.append(ship.move(random.choice(game_map.get_unsafe_moves(ship.position, yard))))
+            except:
+                command_queue.append(ship.move((0,0)))
+        elif game_map[ship.position].halite_amount > constants.MAX_HALITE / 20 and ship.halite_amount < 8*(constants.MAX_HALITE/9):
+            command_queue.append(ship.stay_still())
+        elif goback[ship.id]:
+            if game_map.calculate_distance(ship.position, yard) < 2 and game_map[yard.directional_offset(hlt.Direction.North)].is_occupied and game_map[yard.directional_offset(hlt.Direction.South)].is_occupied and game_map[yard.directional_offset(hlt.Direction.East)].is_occupied and game_map[yard.directional_offset(hlt.Direction.West)].is_occupied:
+                command_queue.append(ship.move(random.choice(game_map.get_unsafe_moves(ship.position, yard))))
+            else:
+                command_queue.append(ship.move(game_map.naive_navigate(ship, yard)))
+
+        elif game_map[ship.position].halite_amount < constants.MAX_HALITE / 15:
+            for dir in random.sample(hlt.Direction.get_all_cardinals(),len(hlt.Direction.get_all_cardinals())):
+                if game_map.calculate_distance(ship.position.directional_offset(dir),yard) > game_map.calculate_distance(ship.position, yard):
+                    command_queue.append(ship.move(game_map.naive_navigate(ship, ship.position.directional_offset(dir))))
+                    break
+        else:
+            command_queue.append(ship.move(game_map.naive_navigate(ship, yard)))
+            
+          # If you're on the first turn and have enough halite, spawn a ship.
+    # Don't spawn a ship if you currently have a ship at port, though.
+    if game.turn_number >= 1 and (me.halite_amount >= constants.SHIP_COST or (len(me.get_ships()) < 1 and me.halite_amount >= constants.SHIP_COST)) and not game_map[me.shipyard].is_occupied:
+            if game.turn_number < constants.MAX_TURNS/2:
+                command_queue.append(game.me.shipyard.spawn())
+            elif len(me.get_ships()) < 1:
+                command_queue.append(game.me.shipyard.spawn())
+    # Send your moves back to the game environment, ending this turn
+    game.end_turn(command_queue)
